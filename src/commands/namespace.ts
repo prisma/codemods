19476bd0562b56d4a785c7b9573f627ab0ee019d
William Luke <william@ordino.ai>
2020-11-09 17:33:18 +0300

    fix: require support

--- src/commands/namespace.ts
+++ src/commands/namespace.ts
@@ -1,37 +1,44 @@
-import {Command, flags} from '@oclif/command'
-import { buildRunner } from '../utils/runner';
-import * as git from '../utils/git'
+import { Command, flags } from "@oclif/command";
+import * as git from "../utils/git";
+import { buildRunner } from "../utils/runner";
 
-const transform =  require.resolve("../transforms/namespace");
+const transform = require.resolve("../transforms/namespace");
 
 export default class Namespace extends Command {
-  static description = 'Codemod for @prisma/client namespace change'
+  static description = "Codemod for @prisma/client namespace change";
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: "h" }),
     // flag with no value (-f, --force)
-    write: flags.boolean({char: 'w', description: 'Write to files', default: false}),
-    debug: flags.boolean({char: 'd', description: 'Debug', default: false})
-  }
+    write: flags.boolean({
+      char: "w",
+      description: "Write to files",
+      default: false,
+    }),
+    debug: flags.boolean({ char: "d", description: "Debug", default: false }),
+  };
 
-  static args = [{name: 'dir'}]
+  static args = [{ name: "dir" }];
 
   async run() {
-    const {args, flags} = this.parse(Namespace)
-    const dir = process.cwd()
-    if(git.isGit(dir)){
-      const result = git.status(dir)
-      if(result.dirty || result.untracked){
-        this.log("You have uncommitted changes. Please commit any changes before running this codemod")
-      }
-      else if(args.dir){
-        this.log(`Performing namespace transform in files ${args.dir} ${flags.write ? '' : 'in dry mode'}`)
-        const runner = buildRunner(transform)
-        const result = await runner(args.dir, {debug: flags.debug, dry: !flags.write})
+    const { args, flags } = this.parse(Namespace);
+    const dir = process.cwd();
+    if (git.isGit(dir)) {
+      const result = git.status(dir);
+      if (args.dir) {
+        this.log(
+          `Performing namespace transform in files ${args.dir} ${
+            flags.write ? "" : "in dry mode"
+          }`
+        );
+        const runner = buildRunner(transform);
+        const result = await runner(args.dir, {
+          debug: flags.debug,
+          dry: !flags.write,
+        });
         console.log(result.stdout);
-  
+        
       }
     }
-    
   }
 }
--- src/transforms/namespace.ts
+++ src/transforms/namespace.ts
@@ -1,8 +1,16 @@
-import { API, JSCodeshift, Collection, FileInfo, Options, CallExpression, Literal, Identifier } from "jscodeshift";
+import {
+  API,
+  CallExpression,
+  Collection,
+  FileInfo,
+  Identifier,
+  JSCodeshift,
+  Options,
+  StringLiteral,
+} from "jscodeshift";
 
-
-function handleImports(root: Collection, j: JSCodeshift){
-  let edit: string[] = []
+function handleImports(root: Collection, j: JSCodeshift) {
+  let edit: string[] = [];
   const prismaImport = root.find(j.ImportDeclaration).filter((nodePath) => {
     // console.log(nodePath.node.source.value);
     return (
@@ -23,55 +31,73 @@ function handleImports(root: Collection, j: JSCodeshift){
     .remove();
 
   specifiers.at(0).insertBefore(j.importSpecifier(j.identifier("Prisma")));
-  return edit
+  return edit;
 }
-function handleRequire(root: Collection, j: JSCodeshift){
-  let edit: string[] = []
+function handleRequire(root: Collection, j: JSCodeshift) {
+  let edit: string[] = [];
+  // console.log("Running");
   root
-  .find(j.VariableDeclarator, {
-    id: {
-      type: "ObjectPattern"
-    },
-    init: {
-      type: "CallExpression",
-      callee: {
-        type: "Identifier",
-        name: "require"
-      },
-      arguments: [
-        {
-          type: "Literal"
+    .find(j.VariableDeclarator)
+    .filter((vdPath) => {
+      // console.log(vdPath);
+      if (
+        vdPath.node.id.type === "ObjectPattern" &&
+        vdPath.node.init &&
+        vdPath.node.init.type === "CallExpression"
+      ) {
+        if (
+          vdPath.node.init.callee.type === "Identifier" &&
+          vdPath.node.init.callee.name === "require"
+        ) {
+          if (vdPath.node.init.arguments) {
+            // console.log(vdPath.node.init.arguments[0].type === "StringLiteral");
+            return true;
+          }
         }
-      ]
-    }
-  })
-  .forEach((path) => {
-    const value = ((path.node.init as CallExpression).arguments[0] as Literal).value
-    if (typeof value === 'string' && value.includes("@prisma/client")) {
-      let variableDeclarator = j(path);
-
-      const properties = variableDeclarator.find(j.Property, { key: { type: "Identifier" } }).forEach((property) => {
-        if ((property.value.key as Identifier).name !== "PrismaClient") {
-          edit.push((property.value.key as Identifier).name);
-          j(property).remove();
-        }
-      });
-      properties.at(0).insertBefore(j.identifier("Prisma"));
-    }
-  });
-  return edit
+      }
+      return false;
+    })
+    .forEach((path) => {
+      const value = ((path.node.init as CallExpression)
+        .arguments[0] as StringLiteral).value;
+      // console.log(value);
+      if (value.includes("@prisma/client")) {
+        let variableDeclarator = j(path);
+        // console.log(path.node.id);
+        const properties = variableDeclarator
+          .find(j.ObjectProperty)
+          .filter((propertyPath) => {
+            // , { key: { type: "Identifier" } }
+            // console.log(propertyPath.node);
+            return propertyPath.node.key.type === "Identifier";
+          })
+          .forEach((property) => {
+            // console.log(property);
+            if ((property.value.key as Identifier).name !== "PrismaClient") {
+              edit.push((property.value.key as Identifier).name);
+              j(property).remove();
+            }
+          });
+        properties.at(0).insertBefore(j.identifier("Prisma"));
+      }
+    });
+  return edit;
 }
 export default function transform(file: FileInfo, api: API, options: Options) {
+  console.log("Running");
   const j = api.jscodeshift;
-
   // Convert the entire file source into a collection of nodes paths.
   const root = j(file.source);
-  // const refs = root.find(j.TSTypeReference)
+
   const importEdits = handleImports(root, j)
-  const requireEdits = handleRequire(root, j)
-  const edit = importEdits.concat(requireEdits).filter(function(elem, pos) {
-    return importEdits.indexOf(elem) == pos;
+  const requireEdits = handleRequire(root, j);
+  
+  const all = importEdits.concat(requireEdits);
+
+  const edit = all.filter(function(elem, pos) {
+    return all.indexOf(elem) == pos;
   })
+  console.log(edit);
   const identifiers = root.find(j.Identifier);
   identifiers
     .filter((idPath) => {
